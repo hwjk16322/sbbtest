d93b82e4cf7755157cad8b691120f975800dd8ea
hwjk16322@gmail.com <hwjk16322@gmail.com>
2023-11-21 13:46:55 +0900

    bb

---
+++ src/main/java/com/std/sbb/answer/Answer.java
@@ -0,0 +1,28 @@
+package com.std.sbb.answer;
+
+import java.time.LocalDateTime;
+
+import com.std.sbb.question.Question;
+import jakarta.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
